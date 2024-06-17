e986c7bb23b3e6131c1d487d66af82e14057197c
bobmagrega <proshanov@yandex.ru>
2024-06-17 18:25:57 +0300

    first commit

--- src/Components/QuestionCard.tsx
+++ src/Components/QuestionCard.tsx
@@ -1,35 +1,54 @@
+import { useEffect, useState } from 'react';
+import ChoiceQuestions from './ChoiceQuestions/ChoiceQuestions';
 import MainButton from './MainButton/MainButton';
 import styles from './QuestionCard.module.css';
+import WrittenQuestions from './WrittenQuestions/WrittenQuestions';
 
-interface QuestionCardProps {
-    question: string;
-    answers: string[];
-    multiple: boolean;
-    isWrittenAnswer: boolean;
-    handleNextQuestion: () => void;
-}
 
-const QuestionCard = ({ question, answers, handleNextQuestion, multiple, isWrittenAnswer }: QuestionCardProps) => {
+const QuestionCard = ({ multiple }: { multiple?: boolean }) => {
+    const [questions, setQuestions] = useState<string[]>([]);
+    const [answers, setAnswers] = useState<string[]>([]);
+    const [questionNum, setQuestionNum] = useState(0);
+    const [commentsBatchNum, setCommentsBatchNum] = useState(0);
+
+    const handleNextQuestion = () => {
+        setQuestionNum(prev => prev + 1);
+        setCommentsBatchNum(prev => prev + 4);
+    }
+
+    useEffect(() => {
+        const api = `https://jsonplaceholder.typicode.com/`
+        const questionsUrl = `posts`;
+        const answersUrl = `comments`;
+
+        const fetchTestData = async (url: string) => {
+            const data = await fetch(`${api}${url}`);
+            return await data.json();
+        }
+
+        const fetchQuestions = () => {
+            fetchTestData(questionsUrl)
+                .then(questions => questions.slice(0, 10).map((question: { body: string }) => question.body))
+                .then(questions => setQuestions(questions));
+        }
+
+        const fetchAnswers = () => {
+            fetchTestData(answersUrl)
+                .then(answers => answers.slice(0, 40).map((answers: { body: string }) => answers.body))
+                .then(answers => setAnswers(answers));
+        }
+        fetchQuestions();
+        fetchAnswers();
+
+    }, [])
+
     return (
         <div className={styles.card}>
             <p className="card-question">
-                <strong>{`${question}?`}</strong>
+                <strong>{`${questions}?`}</strong>
             </p>
-            {isWrittenAnswer ?
-                <h1>Written test</h1>
-                :
-                <form>
-                <div className={styles["card-radio-container"]}>
-                    {answers.map((answer, index) => {
-                        return (
-                            <div key={index} className="card-radio">
-                                <input type={multiple ? "checkbox" : "radio"} id="contactChoice1" name="contact" value="answer" />
-                                <label className={styles['radio-label']} htmlFor="contactChoice1">{answer.slice(0, 15)}</label>
-                            </div>)
-                    })}
-                </div>
-                <MainButton handleClick={handleNextQuestion}>Ответить</MainButton>
-            </form>}
+            <ChoiceQuestions answers={answers} multiple/>
+            <MainButton handleClick={handleNextQuestion}>Ответить</MainButton>
         </div>
     )
 }
--- src/Components/Quiz.tsx
+++ src/Components/Quiz.tsx
@@ -4,16 +4,8 @@ import Results from "./Result";
 import styles from './Quiz.module.css';
 
 const Quiz = () => {
-    const [questions, setQuestions] = useState<string[]>([]);
-    const [answers, setAnswers] = useState<string[]>([]);
-    const [questionNum, setQuestionNum] = useState(0);
-    const [commentsBatchNum, setCommentsBatchNum] = useState(0);
     const [timer, setTimer] = useState(900);
-
-    const handleNextQuestion = () => {
-        setQuestionNum(prev => prev + 1);
-        setCommentsBatchNum(prev => prev + 4);
-    }
+    const [questionsTotal, setQuestionsTotal] = useState(10);
 
     useEffect(() => {
         const timerCountdown = setTimeout(() => {
@@ -25,33 +17,6 @@ const Quiz = () => {
         }, 1000);
     }, [timer]);
 
-
-    useEffect(() => {
-        const api = `https://jsonplaceholder.typicode.com/`
-        const questionsUrl = `posts`;
-        const answersUrl = `comments`;
-
-        const fetchTestData = async (url: string) => {
-            const data = await fetch(`${api}${url}`);
-            return await data.json();
-        }
-
-        const fetchQuestions = () => {
-            fetchTestData(questionsUrl)
-                .then(questions => questions.slice(0, 10).map((question: { body: string }) => question.body))
-                .then(questions => setQuestions(questions));
-        }
-
-        const fetchAnswers = () => {
-            fetchTestData(answersUrl)
-                .then(answers => answers.slice(0, 40).map((answers: { body: string }) => answers.body))
-                .then(answers => setAnswers(answers));
-        }
-        fetchQuestions();
-        fetchAnswers();
-
-    }, [])
-
     return (
         <div className={styles['quiz-container']}>
             <div className={styles["quiz-heading-container"]}>
@@ -62,26 +27,18 @@ const Quiz = () => {
             </div>
             <div className={styles['quiz-progress-bar']}>
                 {Array(10).fill('').map((question, index) => {
-                    const isCurrent = index === questionNum ? `${styles['current-question']}` : "";
-                    const isAnswered = index < questionNum ? `${styles.answered}` : "";
+                    // const isCurrent = index === questionNum ? `${styles['current-question']}` : "";
+                    // const isAnswered = index < questionNum ? `${styles.answered}` : "";
 
                     return (
-                        <span key={index} className={`${styles.bar} ${isCurrent} ${isAnswered}`} />
+                        // <span key={index} className={`${styles.bar} ${isCurrent} ${isAnswered}`} />
+                        <span key={index} className={`${styles.bar}`} />
                     )
                 })
                 }
 
-            </div>{
-                questions.length === questionNum ?
-                    <Results /> :
-                    <QuestionCard
-                        question={questions[questionNum]}
-                        answers={answers.slice(commentsBatchNum, commentsBatchNum + 4)}
-                        handleNextQuestion={handleNextQuestion}
-                        multiple={true}
-                        isWrittenAnswer={true}
-                    />
-            }
+            </div>
+            <QuestionCard />
         </div>
     )
 }
